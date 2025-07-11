b10f1347a41e88a66ca9af6d98eba2c7a46633a4
Sakib Malik <maliksakib347@gmail.com>
2025-07-11 23:38:40 +0530

    google site verification  added

--- app/layout.tsx
+++ app/layout.tsx
@@ -36,6 +36,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta name="google-site-verification" content="3fBWM1HmnoFQLD6S61nNMRT1bQ7NJ_2mas2oITm5D5g" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
